66312fbbfce5a7cee200b796c4af525f708d3f6c
yut-kt <yut.kt59@gmail.com>
2018-04-15 02:01:17 +0900

    add: move account

---
+++ a-tour-of-go/methods-and-interfaces/interfaces.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+import "math"
+
+type Abser interface {
+	Abs() float64
+}
+
+func main() {
+	var a Abser
+	f := MyFloat(-math.Sqrt2)
+	v := Vertex{3, 4}
+
+	a = f
+	a = &v
+
+	a = v
+
+	fmt.Println(a.Abs())
+}
+
+type MyFloat float64
+
+func (f Myfloat) Abs() float64 {
+	if f < 0 {
+		return float64(-f)
+	}
+	return float64(f)
+}
+
+type Vertex struct {
+	X, Y float64
+}
+
+func (v *Vertex) Abs() float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
